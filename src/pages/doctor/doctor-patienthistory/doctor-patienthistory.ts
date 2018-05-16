4832ae97cc103c64abbdd88720f35920a7ee8f11
sandakelum <tharooshawn@gmail.com>
2018-05-16 12:16:27 +0530

    added a chart

--- src/pages/doctor/doctor-patienthistory/doctor-patienthistory.ts
+++ src/pages/doctor/doctor-patienthistory/doctor-patienthistory.ts
@@ -1,14 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'page-doctorpatienthistory',
   templateUrl: 'doctor-patienthistory.html'
 })
 export class DoctorPatientHistoryPage {
-patient: any;
+  @ViewChild('areaCanvas') areaCanvas;
+  patient: any;
+  areaChart: any;
+
   constructor(public navCtrl: NavController, public params: NavParams) {
     this.patient = params.get('patient');
+  }
+
+  ionViewDidLoad() {
+    this.areaChart = new Chart(this.areaCanvas.nativeElement, {
+      type: 'line',
+      data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+          {
+            label: "My First dataset",
+            fill: true,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40],
+            spanGaps: false,
+          }
+        ]
+      }
+    });
+
   }
 
 }
