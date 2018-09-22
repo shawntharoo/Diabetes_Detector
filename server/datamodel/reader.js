540b7fe090b0cbb4ef06cee7d35735259b8bfb34
senuraa <senuraa@msn.com>
2018-09-22 18:41:30 +0530

    symptoms reader added

---
+++ server/datamodel/reader.js
@@ -0,0 +1,29 @@
+var dataModel = require("./model");
+exports.reader = function(req, res) {
+  const symptoms = req.body.symptoms;
+  let complicationName = null;
+  if (symptoms.length >= 3) {
+    dataModel.complications.forEach(element => {
+      complicationName = getComplicationName(element, symptoms);
+      if (complicationName != "none") {
+        res.status(200).json({ complication: complicationName });
+      }
+    });
+    if (complicationName == null) {
+      res.status(500).json({ error: "no complication found" });
+    };
+  } else res.status(500).json({ error: "please include at least 3 symptoms" });
+};
+function getComplicationName(modelObject, symptoms) {
+  let count = 0;
+  modelObject.symptoms.forEach(el => {
+    if (symptoms.includes(el)) {
+      count = count + 1;
+    }
+  });
+  if (count >= 3) {
+    return modelObject.name;
+  } else {
+    return "none";
+  }
+}
--- server/index.js
+++ server/index.js
@@ -3,52 +3,54 @@ var bodyParser = require('body-parser');
 
 var app = express();
 var server = require('http').Server(app);
-var timeseries = require("timeseries-analysis");
+var timeseries = require('timeseries-analysis');
 var cors = require('cors');
+
+var dataModelReader = require('./datamodel/reader');
 
 app.use(bodyParser.json({ limit: '50mb', extended: true }));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.use(cors());
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
 });
 var router = express.Router();
 
 var port = 3001;
 server.listen(port);
-console.log("Magic happening on port " + port);
+console.info('Magic happening on port ' + port);
 
 /**
  * User Activities
  */
 router.route('/prediction').post(function (req, res) {
-    let all_data = req.body;
-    var t = new timeseries.main(all_data);
-    var degree = Math.round(all_data.length / 2);
-    // The sin wave
-    // var t = new timeseries.main(timeseries.adapter.sin({cycles:4}));
-    var forecastDatapoint = 14;
-    var coeffs = t.ARMaxEntropy({ degree: degree, data: t.data.slice(0, all_data.length) });
+	let all_data = req.body;
+	var t = new timeseries.main(all_data);
+	var degree = Math.round(all_data.length / 2);
+	// The sin wave
+	// var t = new timeseries.main(timeseries.adapter.sin({cycles:4}));
+	//var forecastDatapoint = 14;
+	var coeffs = t.ARMaxEntropy({ degree: degree, data: t.data.slice(0, all_data.length) });
 
-    var forecast = 0;
-    for (var i = 0; i < coeffs.length; i++) {
-        forecast -= t.data[t.data.length - 1 - i][1] * coeffs[i];
-    }
-    console.log("forecast", forecast);
-    return res.status(200).send({ forecast: forecast });
+	var forecast = 0;
+	for (var i = 0; i < coeffs.length; i++) {
+		forecast -= t.data[t.data.length - 1 - i][1] * coeffs[i];
+	}
+	console.log('forecast', forecast);
+	return res.status(200).send({ forecast: forecast });
 });
 
 /**
  * Test for 200 response.  Useful when setting up Twilio callback.
  */
 router.route('/test').get(function (req, res) {
-    return res.status(200).send({ "connected": true });
+	return res.status(200).send({ 'connected': true });
 });
-
+router.route('/getcomplication').post(dataModelReader.reader);
 /**
  * Prefix all router calls with 'api'
  */
